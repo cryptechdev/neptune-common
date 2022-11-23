8406468561385ecfb4deef8b8251b0ba267a9d3b
Eric Woolsey <ewoolsey@ualberta.ca>
2022-11-22 19:30:31 -0800

    fmt

--- src/math.rs
+++ src/math.rs
@@ -24,31 +24,37 @@ pub fn from_decimal(other: Decimal) -> Decimal256 {
     Decimal256::from_atomics(other.atomics(), other.decimal_places()).unwrap()
 }
 
-#[test]
-fn assert_serialize() {
-    use cosmwasm_std::to_binary;
-    use rand::Rng;
+#[cfg(test)]
+mod tests {
 
-    for _ in 1..1000 {
-        let mut rng = rand::thread_rng();
-        let num_1: u128 = rng.gen();
-        let num_2: u128 = rng.gen();
+    use super::*;
 
-        let big_num_1 = cosmwasm_std::Uint256::from(num_1);
-        let std_num_1 = cosmwasm_std::Uint256::from(num_1);
+    #[test]
+    fn assert_serialize() {
+        use cosmwasm_std::to_binary;
+        use rand::Rng;
 
-        let big_num_2 = cosmwasm_std::Uint256::from(num_2);
-        let std_num_2 = cosmwasm_std::Uint256::from(num_2);
+        for _ in 1..1000 {
+            let mut rng = rand::thread_rng();
+            let num_1: u128 = rng.gen();
+            let num_2: u128 = rng.gen();
 
-        let big_dec = cosmwasm_std::Decimal256::from_ratio(big_num_1, big_num_2);
-        let std_dec = cosmwasm_std::Decimal256::from_ratio(std_num_1, std_num_2);
+            let big_num_1 = cosmwasm_std::Uint256::from(num_1);
+            let std_num_1 = cosmwasm_std::Uint256::from(num_1);
 
-        assert_eq!(to_binary(&big_dec).unwrap(), to_binary(&std_dec).unwrap());
-        assert_eq!(to_binary(&big_num_1).unwrap(), to_binary(&std_num_1).unwrap());
+            let big_num_2 = cosmwasm_std::Uint256::from(num_2);
+            let std_num_2 = cosmwasm_std::Uint256::from(num_2);
+
+            let big_dec = cosmwasm_std::Decimal256::from_ratio(big_num_1, big_num_2);
+            let std_dec = cosmwasm_std::Decimal256::from_ratio(std_num_1, std_num_2);
+
+            assert_eq!(to_binary(&big_dec).unwrap(), to_binary(&std_dec).unwrap());
+            assert_eq!(to_binary(&big_num_1).unwrap(), to_binary(&std_num_1).unwrap());
+        }
     }
-}
 
-#[test]
-fn get_division_or_zero_test() {
-    assert!(div_or_zero(UINT256_ONE, Uint256::zero()).is_zero());
+    #[test]
+    fn get_division_or_zero_test() {
+        assert!(div_or_zero(UINT256_ONE, Uint256::zero()).is_zero());
+    }
 }
