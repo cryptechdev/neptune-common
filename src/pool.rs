dc697c9029704a4ec0f004ba7deafce484c8961f
Eric Woolsey <ewoolsey@ualberta.ca>
2022-11-23 22:50:50 -0800

    fixed rounding errors

--- src/pool.rs
+++ src/pool.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::Uint256;
+use cosmwasm_std::{Decimal256, Uint256};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -24,7 +24,7 @@ impl<'a> PoolMut<'a> {
         let account_shares = &mut account.shares;
 
         let shares_to_issue = shares;
-        let balance_to_issue = shares_to_issue.multiply_ratio(*pool_balance, *pool_shares);
+        let balance_to_issue = shares_to_issue * Decimal256::from_ratio(*pool_balance, *pool_shares);
 
         *account_shares += shares_to_issue;
         *account_principle += balance_to_issue;
@@ -46,7 +46,7 @@ impl<'a> PoolMut<'a> {
         let shares_to_issue = if pool_balance.is_zero() {
             amount
         } else {
-            pool_shares.multiply_ratio(amount, *pool_balance)
+            amount * Decimal256::from_ratio(*pool_shares, *pool_balance)
         };
 
         *account_shares += shares_to_issue;
@@ -70,7 +70,7 @@ impl<'a> PoolMut<'a> {
             shares
         };
 
-        let amount_to_remove = pool_balance.multiply_ratio(shares_to_remove, *pool_shares);
+        let amount_to_remove = shares_to_remove * Decimal256::from_ratio(*pool_balance, *pool_shares);
 
         *account_shares -= shares_to_remove;
         *account_principle = account_principle.saturating_sub(amount_to_remove);
@@ -91,14 +91,12 @@ impl<'a> PoolMut<'a> {
         let mut shares_to_remove = if pool_balance.is_zero() {
             Uint256::zero()
         } else {
-            pool_shares.multiply_ratio(amount_to_remove, *pool_balance)
+            amount_to_remove * Decimal256::from_ratio(*pool_shares, *pool_balance)
         };
 
-        // TODO: this addition is to eliminate rounding errors and ensures accounts
-        // TODO: aren't left with 1u left inside them. Perhaps needs a better way.
-        if shares_to_remove + Uint256::from(2u64) > *account_shares {
+        if shares_to_remove > *account_shares {
             shares_to_remove = *account_shares;
-            amount_to_remove = pool_balance.multiply_ratio(shares_to_remove, *pool_shares);
+            amount_to_remove = shares_to_remove * Decimal256::from_ratio(*pool_balance, *pool_shares);
         }
 
         *account_shares -= shares_to_remove;
@@ -121,7 +119,7 @@ impl<'a> PoolMut<'a> {
     }
 
     pub fn get_account_balance(self, account: PoolAccount) -> Uint256 {
-        account.shares.checked_multiply_ratio(*self.balance, *self.shares).unwrap_or_default()
+        account.shares * Decimal256::checked_from_ratio(*self.balance, *self.shares).unwrap_or_default()
     }
 }
 
@@ -151,7 +149,7 @@ impl Pool {
     pub fn decrease_balance(&mut self, amount: Uint256) { self.into_ref().decrease_balance(amount) }
 
     pub fn get_account_balance(&self, account: PoolAccount) -> Uint256 {
-        account.shares.checked_multiply_ratio(self.balance, self.shares).unwrap_or_default()
+        account.shares * Decimal256::checked_from_ratio(self.balance, self.shares).unwrap_or_default()
     }
 }
 
