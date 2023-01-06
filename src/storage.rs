36eb9e6e08eb72de44486ce04db09e2aa034b7f1
Eric Woolsey <ewoolsey@ualberta.ca>
2023-01-06 13:40:28 -0800

    consolidated read_map

--- src/storage.rs
+++ src/storage.rs
@@ -9,24 +9,6 @@ pub const STATE_KEY: &str = "state";
 
 pub fn read_map<
     'a,
-    K: Bounder<'a> + PrimaryKey<'a> + KeyDeserialize<Output = K> + 'static,
-    V: Serialize + DeserializeOwned,
->(
-    deps: Deps, start_after: Option<K>, limit: Option<u32>, map: Map<'a, K, V>,
-) -> Result<Vec<(K, V)>, CommonError> {
-    let start = start_after.map(|key| key.inclusive_bound().unwrap());
-    let vec = match limit {
-        Some(limit) => map
-            .range(deps.storage, start, None, Order::Ascending)
-            .take(limit as usize)
-            .collect::<Result<Vec<(K, V)>, StdError>>()?,
-        None => map.range(deps.storage, start, None, Order::Ascending).collect::<Result<Vec<(K, V)>, StdError>>()?,
-    };
-    Ok(vec)
-}
-
-pub fn read_map_ref<
-    'a,
     K: 'static,
     R: Bounder<'a> + PrimaryKey<'a> + KeyDeserialize<Output = K> + 'a,
     V: Serialize + DeserializeOwned,
