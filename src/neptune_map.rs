5799ac0ed1ab1fc2a7e6acf67dd715677ca0e70b
Eric Woolsey <ewoolsey@ualberta.ca>
2023-01-10 15:40:39 -0800

    map refactor

--- src/neptune_map.rs
+++ src/neptune_map.rs
@@ -1,6 +1,6 @@
 use std::{
-    collections::BTreeMap,
     fmt::Debug,
+    iter::FromIterator,
     ops::{Add, AddAssign, Div, Mul, Sub, SubAssign},
 };
 
@@ -13,41 +13,61 @@ use crate::{
     traits::{KeyVec, Zeroed},
 };
 
+/// A map that uses a vector as its underlying data structure.
 #[cw_serde]
-#[derive(Eq, Shrinkwrap, PartialOrd, Ord)]
+#[derive(Shrinkwrap)]
 #[shrinkwrap(mutable)]
-pub struct NeptuneMap<K: Ord, V>(pub BTreeMap<K, V>);
+pub struct NeptuneMap<K, V>(pub Vec<(K, V)>);
 
 impl<K, V> NeptuneMap<K, V>
 where
-    K: Ord,
+    K: PartialEq + Clone + Debug,
 {
-    pub const fn new() -> Self { Self(BTreeMap::new()) }
+    pub const fn new() -> Self { Self(Vec::new()) }
 
-    pub fn must_get(&self, key: &K) -> CommonResult<&V>
-    where
-        K: Debug,
-    {
+    pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        match self.get_mut(&key) {
+            Some(value_mut) => Some(std::mem::replace(value_mut, value)),
+            None => {
+                self.0.push((key, value));
+                None
+            }
+        }
+    }
+
+    pub fn contains_key(&self, key: &K) -> bool { self.get(key).is_some() }
+
+    pub fn get(&self, key: &K) -> Option<&V> {
+        match self.0.iter().position(|x| &x.0 == key) {
+            Some(index) => Some(&self.0[index].1),
+            None => None,
+        }
+    }
+
+    pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
+        match self.0.iter().position(|x| &x.0 == key) {
+            Some(index) => Some(&mut self.0[index].1),
+            None => None,
+        }
+    }
+
+    pub fn must_get(&self, key: &K) -> CommonResult<&V> {
         self.get(key).ok_or_else(|| CommonError::KeyNotFound(format!("{key:?}")))
     }
 
-    pub fn must_get_mut(&mut self, key: &K) -> CommonResult<&mut V>
-    where
-        K: Debug,
-    {
+    pub fn must_get_mut(&mut self, key: &K) -> CommonResult<&mut V> {
         self.get_mut(key).ok_or_else(|| CommonError::KeyNotFound(format!("{key:?}")))
     }
 
-    pub fn get_mut_or_default(&mut self, key: &K) -> &mut V
+    pub fn get_mut_or_default<'a>(&'a mut self, key: &K) -> &'a mut V
     where
-        K: Debug + Clone,
         V: Default,
     {
-        match self.contains_key(key) {
-            true => self.get_mut(key).unwrap(),
-            false => {
+        match self.0.iter().position(|x| &x.0 == key) {
+            Some(index) => &mut self.0[index].1,
+            None => {
                 self.insert(key.clone(), V::default());
-                self.get_mut(key).unwrap()
+                &mut self.0.last_mut().unwrap().1
             }
         }
     }
@@ -55,20 +75,33 @@ where
     /// multiplies every value in self with the corresponding value in rhs. Returns an error if rhs
     /// is missing a key. Rhs must contain every key in self, but self needs not contain every key
     /// in rhs.
+    /// ```
+    /// # use neptune_common::neptune_map::NeptuneMap;
+    /// let quantity: NeptuneMap<_, _> = vec![("cars", 2.0), ("bikes", 3.0)].into();
+    /// let prices: NeptuneMap<_, _> = vec![("cars", 2.0), ("bikes", 1.0)].into();
+    /// let values = quantity.mul_all(&prices).unwrap();
+    /// assert_eq!(values, vec![("cars", 4.0), ("bikes", 3.0)].into());
+    /// ```
     pub fn mul_all<U>(self, rhs: &NeptuneMap<K, U>) -> CommonResult<NeptuneMap<K, <V as Mul<U>>::Output>>
     where
-        K: Debug,
         V: Mul<U>,
         U: Clone,
     {
-        self.into_iter()
-            .map(|(key, val)| {
-                let value = val * rhs.must_get(&key)?.clone();
-                Ok((key, value))
-            })
-            .collect::<Result<NeptuneMap<_, _>, _>>()
+        let mut output = Vec::with_capacity(self.len());
+        for (key, lhs_val) in self {
+            let rhs_val = rhs.must_get(&key)?.clone();
+            output.push((key, lhs_val * rhs_val))
+        }
+        Ok(output.into())
     }
 
+    /// Sums all values in the map.
+    /// ```
+    /// # use neptune_common::neptune_map::NeptuneMap;
+    /// let this: NeptuneMap<_, _> = vec![("cars", 2), ("bikes", 3)].into();
+    /// let total = this.sum();
+    /// assert_eq!(total, 5);
+    /// ```
     pub fn sum(&self) -> V
     where
         V: Default + Add<Output = V> + Clone,
@@ -77,75 +110,70 @@ where
     }
 }
 
-impl<K, V> Default for NeptuneMap<K, V>
-where
-    K: Ord,
-{
-    fn default() -> Self { Self::new() }
+impl<K, V> Default for NeptuneMap<K, V> {
+    fn default() -> Self { Self(Vec::new()) }
 }
 
-impl<K, V> FromIterator<(K, V)> for NeptuneMap<K, V>
-where
-    K: Ord,
-{
-    fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self { Self(BTreeMap::<K, V>::from_iter(iter)) }
+impl<K, V> FromIterator<(K, V)> for NeptuneMap<K, V> {
+    fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self { Vec::<(K, V)>::from_iter(iter).into() }
 }
 
-impl<K, V> IntoIterator for NeptuneMap<K, V>
-where
-    K: Ord,
-{
-    type IntoIter = <BTreeMap<K, V> as IntoIterator>::IntoIter;
+impl<K, V> IntoIterator for NeptuneMap<K, V> {
+    type IntoIter = <Vec<(K, V)> as IntoIterator>::IntoIter;
     type Item = (K, V);
 
     fn into_iter(self) -> Self::IntoIter { self.0.into_iter() }
 }
 
-impl<'a, K, V> IntoIterator for &'a NeptuneMap<K, V>
-where
-    K: Ord,
-{
-    type IntoIter = <&'a BTreeMap<K, V> as IntoIterator>::IntoIter;
-    type Item = (&'a K, &'a V);
+impl<'a, K, V> IntoIterator for &'a NeptuneMap<K, V> {
+    type IntoIter = <&'a Vec<(K, V)> as IntoIterator>::IntoIter;
+    type Item = &'a (K, V);
 
     fn into_iter(self) -> Self::IntoIter { self.0.iter() }
 }
 
-impl<'a, K, V> IntoIterator for &'a mut NeptuneMap<K, V>
-where
-    K: Ord,
-{
-    type IntoIter = <&'a mut BTreeMap<K, V> as IntoIterator>::IntoIter;
-    type Item = (&'a K, &'a mut V);
+impl<'a, K, V> IntoIterator for &'a mut NeptuneMap<K, V> {
+    type IntoIter = <&'a mut Vec<(K, V)> as IntoIterator>::IntoIter;
+    type Item = &'a mut (K, V);
 
     fn into_iter(self) -> Self::IntoIter { self.0.iter_mut() }
 }
 
 impl<K, V> Mul<Decimal256> for NeptuneMap<K, V>
 where
-    K: Ord + PartialEq + Clone + Debug,
+    K: PartialEq + Clone + Debug,
     V: Mul<Decimal256, Output = V> + Clone,
 {
     type Output = Self;
 
     /// multiplies each value with a Decimal256
-    fn mul(self, rhs: Decimal256) -> Self::Output { self.into_iter().map(|(key, val)| (key, val * rhs)).collect() }
+    fn mul(mut self, rhs: Decimal256) -> Self::Output {
+        for (_, val) in &mut self {
+            *val = val.clone() * rhs
+        }
+        self
+    }
 }
 
 impl<K, V> Div<Decimal256> for NeptuneMap<K, V>
 where
-    K: Ord + PartialEq + Clone + Debug,
+    K: PartialEq + Clone + Debug,
     V: Div<Decimal256, Output = V> + Clone,
 {
     type Output = Self;
 
     /// Divides each value with a Decimal256
-    fn div(self, rhs: Decimal256) -> Self::Output { self.into_iter().map(|(key, val)| (key, val / rhs)).collect() }
+    fn div(mut self, rhs: Decimal256) -> Self::Output {
+        for (_, val) in &mut self {
+            *val = val.clone() / rhs
+        }
+        self
+    }
 }
 
 impl<K, V> Add for NeptuneMap<K, V>
 where
-    K: Ord + PartialEq + Clone + Debug,
+    K: PartialEq + Clone + Debug,
     V: Add<Output = V> + Clone + Default,
 {
     type Output = Self;
@@ -163,7 +191,7 @@ where
 
 impl<K, V> AddAssign for NeptuneMap<K, V>
 where
-    K: Ord + PartialEq + Clone + Debug,
+    K: PartialEq + Clone + Debug,
     V: Add<Output = V> + Clone + Default,
 {
     /// Adds the corresponding values from two maps together.
@@ -178,7 +206,7 @@ where
 
 impl<K, V> Sub for NeptuneMap<K, V>
 where
-    K: Ord + PartialEq + Clone + Debug,
+    K: PartialEq + Clone + Debug,
     V: Sub<Output = V> + Clone + Default,
 {
     type Output = Self;
@@ -196,7 +224,7 @@ where
 
 impl<K, V> SubAssign for NeptuneMap<K, V>
 where
-    K: Ord + PartialEq + Clone + Debug,
+    K: PartialEq + Clone + Debug,
     V: Sub<Output = V> + Clone + Default,
 {
     /// Subs the corresponding values from two maps together.
@@ -209,52 +237,36 @@ where
     }
 }
 
-impl<K, V> From<Vec<(K, V)>> for NeptuneMap<K, V>
-where
-    K: Ord,
-{
-    fn from(object: Vec<(K, V)>) -> Self { object.into_iter().collect() }
+impl<K, V> From<Vec<(K, V)>> for NeptuneMap<K, V> {
+    fn from(object: Vec<(K, V)>) -> Self { Self(object) }
 }
 
-impl<K, V> From<(K, V)> for NeptuneMap<K, V>
-where
-    K: Ord,
-{
-    fn from(object: (K, V)) -> Self {
-        let mut output = Self::new();
-        output.insert(object.0, object.1);
-        output
-    }
+impl<K, V> From<(K, V)> for NeptuneMap<K, V> {
+    fn from(object: (K, V)) -> Self { Self(vec![object]) }
 }
 
 impl<K, V> Zeroed for NeptuneMap<K, V>
 where
-    K: Ord,
     V: Zeroed,
 {
     fn is_zeroed(&self) -> bool { self.iter().all(|x| x.1.is_zeroed()) }
 
     fn remove_zeroed(&mut self) {
         self.iter_mut().for_each(|x| x.1.remove_zeroed());
-        self.retain(|_, val| !val.is_zeroed())
+        self.retain(|x| !x.1.is_zeroed())
     }
 }
 
 impl<K, V> KeyVec<K> for NeptuneMap<K, V>
 where
-    K: Ord + PartialEq + Clone,
+    K: PartialEq + Ord + Clone,
 {
-    fn key_vec(&self) -> Vec<K> { self.iter().map(|(key, _)| key.clone()).collect() }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test() {
-        let mut map = NeptuneMap::new();
-        map.insert("Hello", "world");
-        assert!(map.contains_key("Hello"));
+    fn key_vec(&self) -> Vec<K> {
+        let mut all = self.iter().map(|(key, _)| key.clone()).collect::<Vec<_>>();
+        all.sort_unstable();
+        all.dedup();
+        all
     }
 }
+
+// TODO: Unit tests for everything in here
