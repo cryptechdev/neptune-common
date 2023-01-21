13a882a0a786a342b5c9e12da9c14f520c4c1369
Eric Woolsey <ewoolsey@ualberta.ca>
2023-01-21 10:37:48 -0800

    added read map vec

--- src/storage.rs
+++ src/storage.rs
@@ -2,7 +2,7 @@ use cosmwasm_std::{Deps, Order};
 use cw_storage_plus::{Bounder, KeyDeserialize, Map, PrimaryKey};
 use serde::{de::DeserializeOwned, Serialize};
 
-use crate::error::CommonError;
+use crate::{error::CommonError, neptune_map::NeptuneMap};
 
 pub const PARAMS_KEY: &str = "params";
 pub const STATE_KEY: &str = "state";
@@ -27,4 +27,20 @@ pub fn read_map<
         None => map.range(deps.storage, start, None, Order::Ascending).collect::<Result<Vec<_>, _>>()?,
     };
     Ok(vec)
+}
+
+/// Reads a map from storage is ascending order.
+///
+/// TODO: Doc Test Here
+pub fn read_map_vec<
+    'a,
+    // K: 'static,
+    K: Copy + Bounder<'a> + PrimaryKey<'a> + KeyDeserialize<Output = K> + 'a,
+    V: Serialize + DeserializeOwned,
+>(
+    deps: Deps, map: Map<'a, K, V>, vec: Vec<K>,
+) -> Result<NeptuneMap<K, V>, CommonError> {
+    vec.into_iter()
+        .map(|key| Ok((key, map.load(deps.storage, key)?)))
+        .collect::<Result<NeptuneMap<_, _>, CommonError>>()
 }
