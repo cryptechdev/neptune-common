25623ec10bcd76163cf33e0fa8a71a14cd1094a6
Eric Woolsey <ewoolsey@ualberta.ca>
2023-06-22 17:19:34 -0700

    added generics

--- src/send_asset.rs
+++ src/send_asset.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{to_binary, Addr, BankMsg, Binary, Coin, CosmosMsg, Uint256, WasmMsg};
+use cosmwasm_std::{to_binary, Addr, BankMsg, Binary, Coin, CosmosMsg, Uint256, WasmMsg, CustomMsg};
 use cw20::Cw20ExecuteMsg;
 
 use crate::{
@@ -11,7 +11,7 @@ pub type SendFundsMsg = AssetInfo;
 
 /// Transfers both tokens and native tokens to the recipient.
 /// If the amount is zero, it is not included in the returned messages.
-pub fn transfer_assets(recipient: &Addr, mut assets: AssetMap<Uint256>) -> Result<Vec<CosmosMsg>, CommonError> {
+pub fn transfer_assets<C: CustomMsg>(recipient: &Addr, mut assets: AssetMap<Uint256>) -> Result<Vec<CosmosMsg<C>>, CommonError> {
     let mut msgs = vec![];
     // remove any elements that are zero
     assets.remove_zeroed();
@@ -44,19 +44,19 @@ pub fn send_assets(
 
 /// Transfers native tokens to the recipient.
 /// Does not check if the amount is zero.
-fn transfer_coins(coins: Vec<Coin>, recipient_addr: &Addr) -> CosmosMsg {
+fn transfer_coins<C: CustomMsg>(coins: Vec<Coin>, recipient_addr: &Addr) -> CosmosMsg<C> {
     CosmosMsg::Bank(BankMsg::Send { to_address: recipient_addr.to_string(), amount: coins })
 }
 
 /// Sends native tokens to the recipient along with a message.
 /// Does not check if the amount is zero.
-fn send_coins(coins: Vec<Coin>, recipient_addr: &Addr, exec_msg: Binary) -> CosmosMsg {
+fn send_coins<C: CustomMsg>(coins: Vec<Coin>, recipient_addr: &Addr, exec_msg: Binary) -> CosmosMsg<C> {
     CosmosMsg::Wasm(WasmMsg::Execute { contract_addr: recipient_addr.to_string(), msg: exec_msg, funds: coins })
 }
 
 /// Transfers tokens to the recipient.
 /// Does not check if the amount is zero.
-fn transfer_token(token_addr: &Addr, token_amount: Uint256, recipient_addr: &Addr) -> Result<CosmosMsg, CommonError> {
+fn transfer_token<C: CustomMsg>(token_addr: &Addr, token_amount: Uint256, recipient_addr: &Addr) -> Result<CosmosMsg<C>, CommonError> {
     Ok(CosmosMsg::Wasm(WasmMsg::Execute {
         contract_addr: token_addr.to_string(),
         funds: vec![],
