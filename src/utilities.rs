b83eba1e72d4808f91ecd7280faef7c7aab5611d
Eric Woolsey <ewoolsey@ualberta.ca>
2023-06-28 18:15:42 -0700

    added generics

--- src/utilities.rs
+++ src/utilities.rs
@@ -23,7 +23,7 @@ pub fn assert_no_multiple_tx(last_tx_height: &mut u64, current_block_height: u64
 }
 
 /// Sends a message to the contract itself.
-pub fn msg_to_self<ExecuteMsg: Serialize + DeserializeOwned>(env: &Env, msg: &ExecuteMsg) -> CommonResult<CosmosMsg> {
+pub fn msg_to_self<ExecuteMsg: Serialize + DeserializeOwned, M>(env: &Env, msg: &ExecuteMsg) -> CommonResult<CosmosMsg<M>> {
     Ok(CosmosMsg::Wasm(WasmMsg::Execute {
         contract_addr: env.contract.address.to_string(),
         funds: vec![],
